63f2cc5cdb77c089f61f8346fbf2c1f69416eb1b
NikolayIvanovWS <kolyan_ivanov@list.ru>
2024-12-17 14:07:08 +0300

    Version 1.2.4: Added README.md, updated checksum, added shutdown after completion.

--- scripts/service_configuration.py
+++ scripts/service_configuration.py
@@ -3,9 +3,9 @@ import rospy
 import os
 from datetime import datetime
 import time
+import sys
 
-
-configuration_number = "4xT98765"
+configuration_number = "4xT98766"  
 
 def main():
     rospy.init_node('service_configuration', anonymous=True)
@@ -13,21 +13,19 @@ def main():
     rospy.loginfo("Service package 2: Starting configuration...")
     rospy.sleep(1)
 
-
     system_info = os.popen("uname -a").read()
     rospy.loginfo("System Info: " + system_info.strip())
     rospy.sleep(1)
 
-
-    rospy.loginfo("Displaying current date and time for 10 seconds:")
-    for _ in range(10):  
+    rospy.loginfo("Displaying current date and time for 5 seconds:")
+    for _ in range(5):
         current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         rospy.loginfo("Current Time: " + current_time)
         time.sleep(1)
 
-
     rospy.loginfo("Service package 2: Configuration checksum : {}".format(configuration_number))
-    rospy.spin()
+    
+    rospy.signal_shutdown("Script completed successfully")
 
 if __name__ == '__main__':
     main()

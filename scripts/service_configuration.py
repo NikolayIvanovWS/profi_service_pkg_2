466582e9009d238c23c5bc7fbc82af4810a666f9
NikolayIvanovWS <kolyan_ivanov@list.ru>
2024-12-17 13:56:31 +0300

    Release version 1.1.0: Added real-time clock functionality.

--- scripts/service_configuration.py
+++ scripts/service_configuration.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 import rospy
 import os
+from datetime import datetime
+import time
 
-configuration_number = "1xM18652"
+
+configuration_number = "4xT98765"
 
 def main():
     rospy.init_node('service_configuration', anonymous=True)
@@ -10,12 +13,20 @@ def main():
     rospy.loginfo("Service package 2: Starting configuration...")
     rospy.sleep(1)
 
+
     system_info = os.popen("uname -a").read()
     rospy.loginfo("System Info: " + system_info.strip())
-
     rospy.sleep(1)
-    rospy.loginfo("Service package 2: Configuration checksum : {}".format(configuration_number))
 
+
+    rospy.loginfo("Displaying current date and time for 10 seconds:")
+    for _ in range(10):  
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        rospy.loginfo("Current Time: " + current_time)
+        time.sleep(1)
+
+
+    rospy.loginfo("Service package 2: Configuration checksum : {}".format(configuration_number))
     rospy.spin()
 
 if __name__ == '__main__':

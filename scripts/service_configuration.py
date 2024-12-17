b60764f67f0a905b83ab11259328f98131a906c8
NikolayIvanovWS <kolyan_ivanov@list.ru>
2024-12-17 13:41:19 +0300

    Version 1.0.0: Added system info output.

--- scripts/service_configuration.py
+++ scripts/service_configuration.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 import rospy
+import os
 
-# Инициализация переменных
-configuration_number = "1xM00001"
+configuration_number = "1xM18652"
 
 def main():
     rospy.init_node('service_configuration', anonymous=True)
+
+    rospy.loginfo("Service package 2: Starting configuration...")
+    rospy.sleep(1)
+
+    system_info = os.popen("uname -a").read()
+    rospy.loginfo("System Info: " + system_info.strip())
+
+    rospy.sleep(1)
     rospy.loginfo("Service package 2: Configuration checksum : {}".format(configuration_number))
+
     rospy.spin()
 
 if __name__ == '__main__':
